9476fce845702f36dfbac691b9638a32191e2816
christian iverson <chris.w.iverson@gmail.com>
2020-12-17 16:10:10 -0800

    commit tree 3475576e80b581be8e5769e6d39ba4f368d8bef0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
-
+require "pry"
+class CashRegister
+    def initialize(discount=0)
+        @total=0
+        @discount=discount
+        @items= []
+        @price_log=[]
+    end
+    def discount
+        @discount
+    end
+    attr_accessor :total
+    def add_item(title, price, quantity=0)
+      i=0
+        if quantity<1
+        @total += price
+        @items<<title
+        @price_log<<price
+        puts "#{title} = #{price}"
+       else
+        @total += price*quantity
+        while i<quantity
+            @items<<title
+            i+=1
+        end
+        @price_log<<(price*quantity)
+        puts "#{title} = #{price} * #{quantity}"
+       end
+    end
+    def apply_discount
+        if @discount>0
+        discount= 100-@discount
+        @total= (@total/100)*discount
+        "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    def items
+        @items
+    end
+    def void_last_transaction
+        void=@price_log.pop
+       
+        @total=(@total-void)
+    end
+end
